7ee7115e1d120b560bceaddfee680c0f2b77461b
digitalus00 <digitalus002@gmail.com>
2026-06-28 20:19:21 +0545

    GLTTV Try

---
+++ GLTTV/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    // Make sure the Cloudstream extension plugin is applied here
+    id("com.lagradost.cloudstream3.gradle") // Use the template's current version
+}
+
+version = 1
+
+cloudstream {
+    authors     = listOf("Digital")
+    language    = "en"
+    description = "GLT Tv"
+    status      = 1
+    tvTypes     = listOf("All")
+    iconUrl     = "https://pinaycum.tv/favicon.ico"
+}
