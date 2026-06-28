de5114fbbbf0ee04aea7b514a209744708bc0f07
digitalus00 <digitalus002@gmail.com>
2026-06-28 22:39:39 +0545

    new plugin added

---
+++ DesiKahaniya/build.gradle.kts
@@ -0,0 +1,19 @@
+// use an integer that increments with every release
+version = 1
+
+cloudstream {
+    language = "hi"
+    description = "DesiKahani2 – Desi adult video site"
+    authors = listOf("Digital")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     **/
+    status = 1
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?domain=desikahani2.net&sz=%size%"
+}
